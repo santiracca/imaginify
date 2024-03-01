e695763e9692bda91fa8809bcd579a461f487416
Santi Racca <santiagoracca7@gmail.com>
2024-02-29 18:50:45 -0800

    Add authentication pages and middleware

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return <main className='auth'>{children}</main>;
 };
 
 export default Layout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='h-screen'>
+      <p>Home</p>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 };
 
 export default Home;
