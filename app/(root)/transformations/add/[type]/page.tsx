4328490f448c0695bbe6cb3e8f264d8c959adeba
Santi Racca <santiagoracca7@gmail.com>
2024-03-04 18:09:05 -0800

    Add new components and dependencies

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,16 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformationType = transformationTypes[type];
+  return (
+    <>
+      <Header title={transformationType.title} subtitle={transformationType.subTitle} />
+      <TransformationForm />
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
