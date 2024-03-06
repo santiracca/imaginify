e97e17cc814a462254f4917b6ceb78c4720af76c
Santi Racca <santiagoracca7@gmail.com>
2024-03-05 16:45:06 -0800

    Add @radix-ui/react-select and input component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,13 +2,28 @@ import Header from "@/components/shared/Header";
 import React from "react";
 import { transformationTypes } from "@/constants";
 import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
   const transformationType = transformationTypes[type];
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
   return (
     <>
       <Header title={transformationType.title} subtitle={transformationType.subTitle} />
-      <TransformationForm />
+      <section className='mt-10'>
+        <TransformationForm
+          action='Add'
+          userId={user._id}
+          type={transformationType.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   );
 };
